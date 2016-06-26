8e72edea3877828946c388599e0e15a70aeaf166
xordan <mzmudziak@gmail.com>
2016-06-26 22:46:56 +0200

    Added Composite pattern

---
+++ Composite/Composite/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+	class Employee
+	{
+		private string name;
+		private string department;
+		private int salary;
+		private List<Employee> subordinates;
+
+		public Employee(string name, string department, int salary)
+		{
+			this.name = name;
+			this.department = department;
+			this.salary = salary;
+			subordinates = new List<Employee>();
+		}
+
+		public void addSubordinate(Employee employee)
+		{
+			subordinates.Add(employee);
+		}
+
+		public void removeSubordinate(Employee employee)
+		{
+			subordinates.Remove(employee);
+		}
+
+		public List<Employee> getSubordinates()
+		{
+			return subordinates;
+		}
+
+		public override string ToString()
+		{
+			return department+ ":: " + name+ ", salary: " + salary;
+		}
+	}
+}
--- Composite/Composite/Program.cs
+++ Composite/Composite/Program.cs
@@ -10,6 +10,37 @@ namespace Composite
 	{
 		static void Main(string[] args)
 		{
+
+			Employee CEO = new Employee("Mark Zuckerberg", "CEO", 501239432);
+			Employee Director = new Employee("Michal Zmudziak", "Director of Marketing", 1000);
+			Employee headSales = new Employee("Anthony Bourdai","Head Sales", 3032);
+
+			Employee coolGuy1= new Employee("John Andy", "Marketing", 300);
+			Employee coolGuy2 = new Employee("Enry Goey", "Marketing", 340);
+
+			Employee coolGirl1 = new Employee("Patricia Queen","Sales", 4030);
+			Employee coolGirl2 = new Employee("Silvia Angry", "Sales", 49153);
+
+			CEO.addSubordinate(Director);
+			CEO.addSubordinate(headSales);
+
+			Director.addSubordinate(coolGuy1);
+			Director.addSubordinate(coolGuy2);
+
+			headSales.addSubordinate(coolGirl1);
+			headSales.addSubordinate(coolGirl2);
+
+			Console.WriteLine(CEO);
+			foreach (Employee head in CEO.getSubordinates())
+			{
+				Console.WriteLine(head);
+				foreach (Employee subordinate in head.getSubordinates())
+				{
+					Console.WriteLine(subordinate);
+				}
+				Console.WriteLine();
+			}
+
 		}
 	}
 }
