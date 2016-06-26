3244933d749c77eed089023fdc7363e6fc87379b
xordan <mzmudziak@gmail.com>
2016-06-26 23:18:40 +0200

    Added flyweight

---
+++ Flyweight/Flyweight/FlyweightShapeFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+	class FlyweightShapeFactory
+	{
+		private static Dictionary<string, Shape> shapeRepository = new Dictionary<string, Shape>();
+
+		public static Shape GetCircle(string color)
+		{
+			Shape circle;
+			shapeRepository.TryGetValue(color, out circle);
+
+		
+			if (circle == null)
+			{
+				circle = new Circle(color);
+				shapeRepository.Add(color, circle);
+				Console.WriteLine("Created " + circle + " with color " + color);
+			}
+
+			return circle;
+		}
+	}
+}
--- Flyweight/Flyweight/Program.cs
+++ Flyweight/Flyweight/Program.cs
@@ -8,8 +8,39 @@ namespace Flyweight
 {
 	class Program
 	{
+	static	Random random = new Random();
+		private static string[] colors =
+		{
+			"red",
+			"blue",
+			"green",
+			"white",
+			"black"
+		};
+
 		static void Main(string[] args)
 		{
+			for (int i = 0; i < 20; i++)
+			{
+				Circle circle = (Circle) FlyweightShapeFactory.GetCircle(getRandomColor());
+				circle.setX(getRandomCoordinate());
+				circle.setY(getRandomCoordinate());
+				circle.setRadius(30);
+				circle.Draw();
+			}
+
 		}
+
+		private static string getRandomColor()
+		{
+			
+			return colors[random.Next(colors.Length)];
+		}
+
+		private static int getRandomCoordinate()
+		{
+			return random.Next(100);
+		}
+
 	}
 }
